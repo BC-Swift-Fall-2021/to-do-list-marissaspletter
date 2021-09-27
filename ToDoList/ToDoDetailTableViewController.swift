0b021ed82d45877513cc8c9ed2ebca05a5df028d
Marissa <marissa.spletter@yahoo.com>
2021-09-27 15:57:17 -0400

    one

---
+++ ToDoList/ToDoDetailTableViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ToDoDetailTableViewController.swift
+//  ToDoList
+//
+//  Created by Marissa Spletter on 9/26/21.
+//
+
+import UIKit
+
+class ToDoDetailTableViewController: UITableViewController {
+
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var noteView: UITextView!
+    
+    var toDoItem: ToDoItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //edit
+        if toDoItem == nil {
+            toDoItem = ToDoItem(name: "", date: Date().addingTimeInterval(24*60*60), notes: "", reminderSet: false, completed: false)
+            nameField.becomeFirstResponder()
+        
+        
+            nameField.text = toDoItem.name
+            datePicker.date = toDoItem.date
+            noteView.text = toDoItem.notes
+
+}
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        toDoItem = ToDoItem(name: nameField.text!, date: datePicker.date, notes: noteView.text, reminderSet: reminderSwitch.isOn, completed: toDoItem.completed)
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+    }
+}
+
---
+++ ToDoList/ToDoItem.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoItem.swift
+//  ToDoList
+//
+//  Created by Marissa Spletter on 9/27/21.
+//
+
+import Foundation
+
+
+struct ToDoItem: Codable {
+    var name: String
+    var date: Date
+    var notes: String
+    var reminderSet: Bool
+    var notificationID: String?
+    var completed: Bool
+}
